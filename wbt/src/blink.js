fe203aab966febbf252b1f1534bcea5dcd869a5d
Harrison Pearl <hpearl@Harrisons-MacBook-Pro.local>
2022-10-06 20:40:16 -0700

    bingo boingo first commito

---
+++ wbt/src/blink.js
@@ -0,0 +1,11 @@
+// const { Board, Led } = require("johnny-five");
+// import Board from johnny-five
+// import Led from johnny-five
+const board = new Board();
+
+board.on("ready", () => {
+  const led = new Led(13);
+
+  // "blink" the led in 500ms on-off phase periods
+  led.blink(500);
+});
