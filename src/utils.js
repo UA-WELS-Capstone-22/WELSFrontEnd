948a6bc29de27eafcd9f223ab08c14321f960a88
Harrison Pearl <hpearl@arizona.edu>
2023-04-25 19:20:12 -0700

    cur working v

--- src/utils.js
+++ src/utils.js
@@ -41,6 +41,7 @@ function parseData(caller,data){
     case 0b00000:
       // self test // justs needs to know if pass or fail. t/f works
       return parseSelfTest(data[1]);
+
     case 0b00001:
       //Serial number // needs to return string to be updated (needs WBTList)
       return parseSerialNumber(data,"parseFunc");
@@ -59,11 +60,13 @@ function parseData(caller,data){
       return true;
     case 0b00100:
       // charge cont.  // needs to return string to be updated (needs WBTList)
+      caller.WBTs[addr-1].updateStatus("Charging");
       console.log(addr);
       caller.WBTs[addr-1].updateData(standardParse(data));;
       break;
     case 0b00101:
       // impedance  // returns impedance
+      caller.WBTs[addr-1].updateStatus("running impedance test");
       let impd = ImpedanceParse(data)
       console.log(impd);
       caller.WBTs[addr-1].updateData(impd);
@@ -71,26 +74,30 @@ function parseData(caller,data){
       break;
     case 0b00110:
       // trip test // returns time
+      caller.WBTs[addr-1].updateStatus("trip test running");
       let ttest = tripTestParse(data)
       caller.WBTs[addr-1].updateData(ttest); 
       caller.WBTs[addr-1].WBTData["Trip_Test_Time"] = ttest["Trip_Test_Time"];
       break;
     case 0b00111:
       // hold test  // needs to return string to be updated (needs WBTList)
+      caller.WBTs[addr-1].updateStatus("Hold test running");
       caller.WBTs[addr-1].updateData(standardParse(data));
       break;
     case 0b01000:
       // full discharge // needs to return string to be updated (needs WBTList)
+      caller.WBTs[addr-1].updateStatus("Discharging");
       caller.WBTs[addr-1].updateData(standardParse(data));
       break;
     case 0b01001:
       // store/ship  // needs to return string to be updated (needs WBTList)
+      caller.WBTs[addr-1].updateStatus("Storing/Shipping");
       caller.WBTs[addr-1].updateData(standardParse(data));
       break;
     case 0b11000:
       // atp complete // generate report & update state to idle?
       caller.WBTs[addr-1].clearData();
-      caller.WBTs[addr-1].updateState("idle"); // maybe add test complete state?
+      caller.WBTs[addr-1].updateStatus("idle"); // maybe add test complete state?
       break;
     case 0b11001:
       // test complete // generate report & updates state to idle?
